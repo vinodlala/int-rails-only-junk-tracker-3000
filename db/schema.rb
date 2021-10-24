a55ffe596a33b1785bc2cfb330c3db95ab5ad779
Vinod Lala <vinod_lala@usa.net>
2021-10-24 19:12:13 -0400

    Add migration for more columns in vehicles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_224014) do
+ActiveRecord::Schema.define(version: 2021_10_24_230526) do
 
   create_table "vehicles", force: :cascade do |t|
     t.string "nickname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "registration_id"
+    t.integer "mileage"
+    t.integer "wheels"
+    t.string "engine_status"
   end
 
 end
