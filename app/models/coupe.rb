c0715795399d102d4050460709acae2dab7a8cfe
Vinod Lala <vinod_lala@usa.net>
2021-10-24 19:18:03 -0400

    Add migration for creating coupes

---
+++ app/models/coupe.rb
@@ -0,0 +1,2 @@
+class Coupe < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_231355) do
+ActiveRecord::Schema.define(version: 2021_10_24_231731) do
+
+  create_table "coupes", force: :cascade do |t|
+    t.integer "doors"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sedans", force: :cascade do |t|
     t.integer "doors"
